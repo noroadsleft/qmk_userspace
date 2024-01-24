15403ecdb90ae67774afd98954bce0144b82a1f0
James Young <18669334+noroadsleft@users.noreply.github.com>
2024-01-24 02:09:17 -0800

    Add `planck/rev6:noroadsleft` keymap
    Imported from `noroadsleft/qmk_firmware@73d226343a`.

---
+++ keyboards/planck/keymaps/noroadsleft/keymap.c
@@ -0,0 +1,118 @@
+/* Copyright 2019-2022 James Young (@noroadsleft)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "noroadsleft.h"
+#include "muse.h"
+
+enum layer_names {
+    _DV,
+    _QW,
+    _CM,
+    _LW,
+    _RS,
+    _SY
+};
+
+enum custom_keycodes {
+    DVORAK = KEYMAP_SAFE_RANGE,
+    QWERTY,
+    COLEMAK
+};
+
+#define CTL_GRV MT(MOD_LCTL, KC_GRV)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [_DV] = LAYOUT_ortho_4x12(
+        KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC,
+        KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH,
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_ENT, 
+        CTL_GRV, KC_NUBS, KC_LGUI, KC_LALT, MO(_LW), KC_SPC,  KC_SPC,  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
+
+    [_QW] = LAYOUT_ortho_4x12(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, 
+        CTL_GRV, KC_NUBS, KC_LGUI, KC_LALT, MO(_LW), KC_SPC,  KC_SPC,  MO(_RS), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    ),
+
+    [_LW] = LAYOUT_ortho_4x12(
+        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+        _______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    ),
+
+    [_RS] = LAYOUT_ortho_4x12(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS,
+        _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+
+    [_SY] = LAYOUT_ortho_4x12(
+        TG(_SY), DVORAK,  QWERTY,  COLEMAK, _______, _______, _______, _______, QK_BOOT, DB_TOGG, VRSN,    _______,
+        _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    )
+
+};
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LW, _RS, _SY);
+}
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case DVORAK:
+            if (record->event.pressed) {
+                layer_move(_DV);
+                set_single_persistent_default_layer(_DV);
+            }
+            return false;
+        case QWERTY:
+            if (record->event.pressed) {
+                layer_move(_QW);
+                set_single_persistent_default_layer(_QW);
+            }
+            return false;
+        case COLEMAK:
+            if (record->event.pressed) {
+                layer_move(_CM);
+                set_single_persistent_default_layer(_CM);
+            }
+            return false;
+    }
+    return true;
+}
+
+void encoder_update(bool clockwise) {
+    if (clockwise) {
+#ifdef MOUSEKEY_ENABLE
+        tap_code(KC_MS_WH_DOWN);
+#else
+        tap_code(KC_PGDN);
+#endif
+    } else {
+#ifdef MOUSEKEY_ENABLE
+        tap_code(KC_MS_WH_UP);
+#else
+        tap_code(KC_PGUP);
+#endif
+    }
+}
