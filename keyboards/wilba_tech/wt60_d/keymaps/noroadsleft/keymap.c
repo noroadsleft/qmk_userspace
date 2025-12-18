9386724797d998eff31874e73aad70b3f9bbf5cd
James Young <18669334+noroadsleft@users.noreply.github.com>
2025-12-18 01:04:22 -0800

    `wilba_tech/wt60_d:noroadsleft`: add mousewheel scroll keys
    - add mouse wheel scroll keys to Function layer
      - enable Mousekeys feature

--- keyboards/wilba_tech/wt60_d/keymaps/noroadsleft/keymap.c
+++ keyboards/wilba_tech/wt60_d/keymaps/noroadsleft/keymap.c
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN] = LAYOUT_60_iso_tsangan_split_rshift(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, KC_CALC, KC_APP,  _______, _______, _______, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PSCR, KC_SCRL,
-        _______, M_SALL,  _______, _______, _______, _______, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, KC_PAUS, KC_PENT,
+        _______, KC_CALC, KC_APP,  _______, _______, MS_WHLU, KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_PSCR, KC_SCRL,
+        _______, M_SALL,  _______, _______, _______, MS_WHLD, KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, KC_PAUS, KC_PENT,
         _______, _______, M_UNDO,  M_CUT,   M_COPY,  M_PASTE, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, TG(_SY), _______, MO(_MA),
         _______, _______, _______,                            TG(_NP),                                     _______, _______, _______
     ),
